20d1b996c7e8e6aa44cfc8c17c32fa3f36151792
bryce <bryce@bozar>
2011-09-05 13:58:37 -0400

    added basic files, removed photo url for now

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 var express = require('express')
+var fs = require('fs')
+var ejs = require('ejs')
+var path = require('path')
+var imagemagick = require('imagemagick')
 
-var app = express.createServer();
+var thumbnails = require('./util/thumbnail')
+
+var photoPath = path.join(__dirname,'/public/organized')
+
+var app = express.createServer()
+//app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 app.get('/', function(req, res) {
-    res.send('Hello World');
-});
+	fs.readdir(photoPath, function(err, files) {
+
+		var pictures = {}
+
+		files.forEach(function(file) {
+			pictures[file] = (fs.readdirSync(path.join(photoPath,file)))
+		})
+
+		res.render("home", {albums:pictures})
+	})
+})
+
+app.get('/test', function(req, res) {
+	res.render("home.ejs")
+})
 
 app.configure(function() {
-	app.use(express.static(__dirname+'/public'))
+	app.use(express.static(path.join(__dirname,'/public')))
 })
 
 app.listen(3000);
