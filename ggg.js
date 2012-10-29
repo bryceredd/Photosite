3d29bf689930daf66bc49fa20b0b9de332324886
bryce redd <bryce@i.tv>
2012-10-28 23:27:30 -0600

    updated bozar

---
+++ ggg.js
@@ -0,0 +1,14 @@
+// example ggg.js. Delete what you don't need
+module.exports = {
+
+  // services
+  start: "node_modules/.bin/coffee app.coffee",
+
+  // install
+  install: "npm install",
+
+  // servers to deploy to
+  servers: {
+    bozar: "root@bozar.dyndns.org",
+  }
+}
